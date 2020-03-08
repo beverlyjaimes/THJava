4be8f3e91438250676bd21cc67773e19de399f40
beverlyjaimes <beverly.jaimespuente@gmail.com>
2020-03-07 23:32:15 -0600

    removed person class used meals

--- src/com/teamtreehouse/Main.java
+++ src/com/teamtreehouse/Main.java
@@ -1,9 +1,16 @@
 package com.teamtreehouse;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("hi");
+        Map<String, String> meals = new HashMap<>();
+        meals.put("breakfast", "Waffles");
+        meals.put("lunch", "Gyros");
+        meals.put("dinner", "Enchiladas");
+
+        System.out.println(meals);
     }
 }
