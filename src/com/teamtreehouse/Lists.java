8b6badd478785b9cc8ca705a104ed081cdd9bf93
beverlyjaimes <beverly.jaimespuente@gmail.com>
2020-03-09 22:49:07 -0500

    added lists class

---
+++ src/com/teamtreehouse/Lists.java
@@ -0,0 +1,11 @@
+package com.teamtreehouse;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lists {
+    public static void main(String[] args) {
+        List<String> groceryLine = new ArrayList<String>();
+    }
+}
