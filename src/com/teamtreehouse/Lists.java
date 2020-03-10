ded55071c7b6a379fde47ca1660c0d308b3d94fb
beverlyjaimes <beverly.jaimespuente@gmail.com>
2020-03-09 22:52:30 -0500

    review remove, get, using index and string

--- src/com/teamtreehouse/Lists.java
+++ src/com/teamtreehouse/Lists.java
@@ -7,5 +7,17 @@ import java.util.List;
 public class Lists {
     public static void main(String[] args) {
         List<String> groceryLine = new ArrayList<String>();
+        groceryLine.add("Jerry");
+        groceryLine.add("Sam");
+        groceryLine.add("Amber");
+
+        System.out.println(groceryLine);
+
+//        groceryLine.remove("Sam");
+        groceryLine.remove(1);
+        System.out.println(groceryLine);
+
+        String jerry = groceryLine.get(0);
+        System.out.println(jerry);
     }
 }
