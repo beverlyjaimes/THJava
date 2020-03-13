a8d037dde6e7e8cff79b77c500b05b3259905f93
beverlyjaimes <beverly.jaimespuente@gmail.com>
2020-03-13 05:38:47 -0500

    scanner review

--- src/com/teamtreehouse/review/HelloJava.java
+++ src/com/teamtreehouse/review/HelloJava.java
@@ -9,6 +9,12 @@ public class HelloJava {
         System.out.println("Enter a random number: ");
         int userNumber = input.nextInt();
         System.out.println("You entered "+ userNumber);
+        Scanner str = new Scanner(System.in);
+        System.out.println("What is your name: " );
+        String userName = input.next();
+        System.out.println("Your name is  "+ userName);
+
+
 
     }
 }
