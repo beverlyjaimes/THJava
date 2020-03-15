46a7206fa76b39bf855c846cade5eba04c50b8f0
beverlyjaimes <beverly.jaimespuente@gmail.com>
2020-03-14 21:14:45 -0500

    missing char

--- src/com/teamtreehouse/review/Bat.java
+++ src/com/teamtreehouse/review/Bat.java
@@ -5,6 +5,7 @@ public class Bat {
         public static void main(String[] args) {
             System.out.println(sleepIn(true, false));
             System.out.println(monkeyTrouble(true, false));
+            System.out.println(missingChar("Kitten", 3));
 
         }
         public static boolean sleepIn(boolean weekday, boolean vacation) {
@@ -20,6 +21,15 @@ public class Bat {
         }
         return false;
     }
+
+    public static String missingChar(String str, int n) {
+        String front = str.substring(0, n);
+
+        String back = str.substring(n + 1, str.length());
+
+        return front + back;
+    }
+
 
     }
 
