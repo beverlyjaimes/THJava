65ec996d77d52e32eb4f112b3035735f37e071fc
beverlyjaimes <beverly.jaimespuente@gmail.com>
2020-03-14 21:09:52 -0500

    monkeyTrouble

--- src/com/teamtreehouse/review/Bat.java
+++ src/com/teamtreehouse/review/Bat.java
@@ -4,11 +4,22 @@ public class Bat {
 
         public static void main(String[] args) {
             System.out.println(sleepIn(true, false));
+            System.out.println(monkeyTrouble(true, false));
 
         }
         public static boolean sleepIn(boolean weekday, boolean vacation) {
             return (vacation) || (!weekday && !vacation);
         }
+
+        public static boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+        if (aSmile && bSmile) {
+            return true;
+        }
+        if (!aSmile && !bSmile) {
+            return true;
+        }
+        return false;
+    }
 
     }
 
