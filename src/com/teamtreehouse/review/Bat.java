c95d53e1834c417dbdd454095ce576fff0d54cd4
beverlyjaimes <beverly.jaimespuente@gmail.com>
2020-03-17 22:26:04 -0500

    stringTimes

--- src/com/teamtreehouse/review/Bat.java
+++ src/com/teamtreehouse/review/Bat.java
@@ -39,5 +39,13 @@ public class Bat {
         }
     }
 
+    public static String stringTimes(String str, int n) {
+        String result = "";
+        for (int i=0; i<n; i++) {
+            result = result + str;  // could use += here
+        }
+        return result;
+    }
+
     }
 
