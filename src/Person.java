ea1bdb733a3865a1ded00b03996cc196f48fe06a
beverlyjaimes <beverly.jaimespuente@gmail.com>
2020-03-18 23:19:42 -0500

    added getter and setter

--- src/Person.java
+++ src/Person.java
@@ -1,4 +1,20 @@
 public class Person {
     private String name;
 
+    public Person(String name){
+        setName(name);
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    private void setName(String name) {
+        this.name = name;
+
+    }
+
+
+
 }
+
