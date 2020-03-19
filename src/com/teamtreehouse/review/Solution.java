ae8c56b09bb49b71785611f8fed5ed03025ed292
beverlyjaimes <beverly.jaimespuente@gmail.com>
2020-03-19 04:33:54 -0500

    review hackerrank problem

--- src/com/teamtreehouse/review/Solution.java
+++ src/com/teamtreehouse/review/Solution.java
@@ -1,4 +1,24 @@
 package com.teamtreehouse.review;
 
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
 public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner input = new Scanner(System.in);
+        int i = 1;
+        while (input.hasNextLine()) {
+            String str = input.nextLine();
+            if(str.isEmpty())
+                break;
+            System.out.println(i + " " +  str);
+            i++;
+        }
+        input.close();
+    }
 }
