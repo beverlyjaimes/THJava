e71851b84f4d570e07c331e2cae1e2160b671218
beverlyjaimes <beverly.jaimespuente@gmail.com>
2020-04-01 20:55:03 -0500

    solution

--- src/com/teamtreehouse/review/Bat.java
+++ src/com/teamtreehouse/review/Bat.java
@@ -109,7 +109,10 @@ public class Bat {
 
 //    We have a loud talking parrot. The "hour" parameter is the current hour time in the range 0..23. We are in trouble if the parrot is talking and the hour is before 7 or after 20. Return true if we are in trouble.
 
+    public static boolean parrotTrouble(boolean talking, int hour) {
+            return (talking && (hour < 7 || hour > 20));
 
+    }
 
 }
 
